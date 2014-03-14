cee6c207da4982dd4811c45365b0daf724b77075
shinriyo <shinriyo@gmail.com>
2014-03-14 22:55:01 +0900

    cleanup
    tab to space

--- pomelo-dotnetClient/src/protocol/Message.cs
+++ pomelo-dotnetClient/src/protocol/Message.cs
@@ -3,20 +3,19 @@ using SimpleJson;
 
 namespace Pomelo.DotNetClient
 {
-	public class Message
-	{
-		public MessageType type;
-		public string route;
-		public uint id;
-		public JsonObject data;
+    public class Message
+    {
+        public MessageType type;
+        public string route;
+        public uint id;
+        public JsonObject data;
 
-		public Message (MessageType type, uint id, string route, JsonObject data)
-		{
-			this.type = type;
-			this.id = id;
-			this.route = route;
-			this.data = data;
-		}
-	}
+        public Message (MessageType type, uint id, string route, JsonObject data)
+        {
+            this.type = type;
+            this.id = id;
+            this.route = route;
+            this.data = data;
+        }
+    }
 }
-
