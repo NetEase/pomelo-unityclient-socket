0695d6c9dbf84f69f7b4c6f02b0ffa6551014fbc
shinriyo <shinriyo@gmail.com>
2014-03-14 22:54:31 +0900

    cleanup
    tab to space

--- pomelo-dotnetClient/src/protocol/HeartBeatService.cs
+++ pomelo-dotnetClient/src/protocol/HeartBeatService.cs
@@ -3,60 +3,68 @@ using System.Timers;
 
 namespace Pomelo.DotNetClient
 {
-	public class HeartBeatService
-	{
-		int interval;
-		public int timeout;
-		Timer timer;
-		DateTime lastTime;
+    public class HeartBeatService
+    {
+        int interval;
+        public int timeout;
+        Timer timer;
+        DateTime lastTime;
+        Protocol protocol;
 
-		Protocol protocol;
-		
-		public HeartBeatService(int interval, Protocol protocol){
-			this.interval = interval*1000;
-			this.protocol = protocol;
-		}
+        public HeartBeatService (int interval, Protocol protocol)
+        {
+            this.interval = interval * 1000;
+            this.protocol = protocol;
+        }
 
-		internal void resetTimeout(){
-			this.timeout = 0;
-			lastTime = DateTime.Now;
-		}
+        internal void resetTimeout ()
+        {
+            this.timeout = 0;
+            lastTime = DateTime.Now;
+        }
 
-		public void sendHeartBeat(object source, ElapsedEventArgs e){
-			//check timeout
-			if(timeout > interval*2){
-				protocol.getPomeloClient().disconnect();
-				stop ();
-				return;
-			}
+        public void sendHeartBeat (object source, ElapsedEventArgs e)
+        {
+            //check timeout
+            if (timeout > interval * 2)
+            {
+                protocol.getPomeloClient ().disconnect ();
+                stop ();
+                return;
+            }
 
-			TimeSpan span = DateTime.Now - lastTime;
-			timeout += (int)span.TotalMilliseconds;
+            TimeSpan span = DateTime.Now - lastTime;
+            timeout += (int)span.TotalMilliseconds;
 
-			//Send heart beat
-			protocol.send(PackageType.PKG_HEARTBEAT);
-		}
+            //Send heart beat
+            protocol.send (PackageType.PKG_HEARTBEAT);
+        }
 
-		public void start(){
-			if(interval < 1000) return;
+        public void start ()
+        {
+            if (interval < 1000)
+            {
+                return;
+            }
 
-			//start hearbeat
-			this.timer = new Timer();
-			timer.Interval = interval;
-			timer.Elapsed += new ElapsedEventHandler(sendHeartBeat);
-			timer.Enabled = true;
+            //start hearbeat
+            this.timer = new Timer ();
+            timer.Interval = interval;
+            timer.Elapsed += new ElapsedEventHandler (sendHeartBeat);
+            timer.Enabled = true;
 
-			//Set timeout
-			timeout = 0;
-			lastTime = DateTime.Now;
-		}
+            //Set timeout
+            timeout = 0;
+            lastTime = DateTime.Now;
+        }
 
-		public void stop(){
-			if(this.timer != null) {
-				this.timer.Enabled = false;
-				this.timer.Dispose();
-			}
-		}
-	}
+        public void stop ()
+        {
+            if (this.timer != null)
+            {
+                this.timer.Enabled = false;
+                this.timer.Dispose ();
+            }
+        }
+    }
 }
-
